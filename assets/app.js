c3faebc9bcecd622937c4643219c02558079f407
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-07-20 11:42:23 -0400

    adding files

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+import "/app.css";
+import search from "./assets/search";
+
+function application() {
+    return (
+        <div class="container">
+            <search />
+        </div>
+    );
+
+}
+
+export default application;
